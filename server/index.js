5d95801ec23605b4f110b35d9e343dd2de8ea9d4
elsalvadordali <tloochich@icloud.com>
2022-05-19 14:44:40 -0400

    socket io server online, but having cors issues??

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3030
 
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(cors())
+
+const io = require('socket.io')(server, {
+  origins: 'http://localhost:8080',
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+io.on('connection', socket => {
+  socket.on('user-clicked', msg => {
+    console.log(msg)
+  })
 })
 
 app.listen(port, () => console.log(`Application listening on port ${port}!`))
